fcb1fd5629a0c0b57ee9c5455db953209ed3ffd5
liubo <liubo@beisen.com>
2015-08-13 17:29:36 +0800

    init

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -4,20 +4,11 @@
 var webpack = require("webpack");
 var	path = require("path");
 
-var reactExternal = {
-    root: 'React',
-    commonjs2: 'react',
-    commonjs: 'react',
-    amd: 'react'
-};
-
-
 module.exports = {
     entry: path.resolve(__dirname, "../src/js/index"),
     output: {
         library: "AppUI",
-        libraryTarget: "commonjs2",
-
+        libraryTarget: "umd",
         path: path.resolve(__dirname, "../dist"),
         filename: "bs-appui.js"
     },
@@ -38,27 +29,8 @@ module.exports = {
         extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx']
     },
     externals: {
-        react: 'react'
+        "react": "react",
+        "react/addons": "react"
     },
     devtool: 'eval'
 };
-
-//var ComponentPlugin = require("component-webpack-plugin");
-//module.exports = {
-//    module: {
-//        loaders: [
-//            {
-//                test: /\.jsx?$/
-//                ,exclude: /node_modules/
-//                ,loader: 'babel'
-//                ,query: {
-//                    optional: ['runtime'],
-//                    stage: 0
-//                }
-//            }
-//        ]
-//    },
-//    plugins: [
-//        new ComponentPlugin()
-//    ]
-//}
