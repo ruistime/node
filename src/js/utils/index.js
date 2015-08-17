a35ff53aab9c3bbf0d8c6c03f70e40cbf8006ddd
liubo <liubo@beisen.com>
2015-08-17 18:21:21 +0800

    init

---
+++ src/js/utils/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by liubo on 15/8/17.
+ */
+//export AnimationEvents from './animationEvents';
+export CSSCore from './cssCore';
+export Events from './events';
+export ImmutabilityHelper from './immutabilityHelper';
+export Modernizr from './modernizr';
+export Styles from './styles';
+export StylePropable from './stylePropable';
+export transitionEvents from './TransitionEvents';
